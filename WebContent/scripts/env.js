d0a1827f76cab7fe4f1748fdce2bf9ac1597d6b0
kaslrca <kaslrca@gmail.com>
2014-09-01 16:28:11 +0800

    asdf

--- WebContent/scripts/env.js
+++ WebContent/scripts/env.js
@@ -2,4 +2,4 @@ var env = angular.module('env', []);
 
 env.constant('env', {'ip':'http://10.1.100.76:88', 'ws':'ws://10.1.100.76:89/websocket'});
 env.constant('action', {'send':'send', 'saveName':'saveName', 'listener':'listener'});
-env.constant('index', Math.floor((Math.random() * 100) + 1));
+env.constant('index', Math.floor((Math.random() * 999) + 1));
